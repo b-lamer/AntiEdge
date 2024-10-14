f891d6fc2f5777ae9c939bdfa13d9db73ab946c4
b-lamer <152201467+b-lamer@users.noreply.github.com>
2024-10-14 17:36:40 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,8 +1,13 @@
-$browseryn = Read-Host "Do you have a browser installed? (Y/N)"
+$browseryn = Read-Host "Do you have a browser besides Microsoft Edge installed? (Y/N)"
 If ($browseryn -eq 'y'){
-    write-host "You got dat browser"
-}ElseIf ($browseryn -eq 'n'){
-    write-host "You missin a browser"
-}Else{
-    write-host "Invalid input"
+    $browseryn = Read-Host "Are you sure? If you continue with the script without a browser you will lose access to the web. (Y/N)"
+} 
+If ($browseryn -eq 'n'){
+    $browseryn = read-host "Would you like the script to install a browser for you? (Y/N)"
+    if ($browseryn -eq 'y'){
+        write-host "Please wait while the script installs a browser for you."
+    } else {
+        write-host "Cancelling script."
+        Exit
+    }
 }
