a05b702a93ba0e924c52498d3cfb16c0530068b9
b-lamer <152201467+b-lamer@users.noreply.github.com>
2024-10-14 19:31:18 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,11 +1,17 @@
-$browseryn = Read-Host "Do you have a browser besides Microsoft Edge installed? (Y/N)"
+$foxurl = "https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US"
+
+$browseryn = Read-Host "Are you sure you want to run this script? 
+If you do not have a browser installed, you may not be able to access the web. (Y/N)"
+
 If ($browseryn -eq 'y'){
-    $browseryn = Read-Host "Are you sure? If you continue with the script without a browser you will lose access to the web. (Y/N)"
-} 
-If ($browseryn -eq 'n'){
+    
+} ElseIf ($browseryn -eq 'n'){
     $browseryn = read-host "Would you like the script to install a browser for you? (Y/N)"
     if ($browseryn -eq 'y'){
         write-host "Please wait while the script installs a browser for you."
+        Invoke-WebRequest -Uri $foxurl -OutFile $env:TEMP\firefox_installer.exe
+        Start-Process -FilePath $env:TEMP\firefox_installer.exe -ArgumentList "/S" -Wait
+        Remove-Item "$env:TEMP\firefox_installer.exe"
     } else {
         write-host "Cancelling script."
         Exit
