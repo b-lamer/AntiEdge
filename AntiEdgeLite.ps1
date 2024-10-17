d90197cdb2351939bd1a3db255f65ec11459d3be
b-lamer <152201467+b-lamer@users.noreply.github.com>
2024-10-16 22:45:11 -0500

    Changes registry value

--- AntiEdgeLite.ps1
+++ AntiEdgeLite.ps1
@@ -1,2 +1,4 @@
 #Lighter version of AntiEdge.ps1, does not install any browsers and only does basic removal
+Set-Location -Path "HKLM:\Software\Microsoft"
 Get-Item -Path "HKLM:\SOFTWARE\Microsoft" | New-Item -Name "EdgeUpdate" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate" -Name "DoNotUpdateToEdgeWithChromium" -Value "1" -PropertyType DWORD -Force
