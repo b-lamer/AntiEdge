fbf048cbd617eb09fe060ffd7e96419b167d723b
b-lamer <152201467+b-lamer@users.noreply.github.com>
2024-10-17 21:40:55 -0500

    Old file, will need to change since MS is dumb

--- AntiEdgeLite.ps1
+++ AntiEdgeLite.ps1
@@ -1,8 +1,8 @@
 #Lighter version of AntiEdge.ps1, does not install any browsers and only does basic removal
+Set-Location -Path "C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer"
+.\setup.exe --uninstall --system-level --verbose-logging --force-uninstall
+
+
 Set-Location -Path "HKLM:\Software\Microsoft"
 Get-Item -Path "HKLM:\SOFTWARE\Microsoft" | New-Item -Name "EdgeUpdate" -Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate" -Name "DoNotUpdateToEdgeWithChromium" -Value "1" -PropertyType DWORD -Force
-
-
-Set-Location -Path "C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer"
-.\setup.exe --uninstall --system-level --verbose-logging --force-uninstall
